850aea968c4aa54edb1611140ed284fba673c917
TarikElha <elhantiti.t@gmail.com>
2022-08-23 04:56:49 +0200

    format date edit/new ok + login/logout ok

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ImageType extends AbstractType
 {
@@ -14,10 +15,14 @@ class ImageType extends AbstractType
     {
         $builder
             ->add('urlFile', VichFileType::class, [
+                'label' => 'Sélectionner une image',
                 'required'      => true,
                 'allow_delete'  => true, // not mandatory, default is true
                 'download_uri' => true, // not mandatory, default is true
             ])
+            /* ->add('position', IntegerType::class, [
+                'label' => 'Position de l\'image dans la gallerie',
+            ]) */
         ;
     }
 
--- src/Form/WebsiteType.php
+++ src/Form/WebsiteType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use App\Entity\Website;
 use Symfony\Component\Form\AbstractType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class WebsiteType extends AbstractType
 {
@@ -24,7 +25,14 @@ class WebsiteType extends AbstractType
                 'allow_delete'  => true, // not mandatory, default is true
                 'download_uri' => true, // not mandatory, default is true
             ])
-            ->add('typeportfolio')
+            ->add('typeportfolio', ChoiceType::class, 
+                [
+                    'choices' => [
+                        'Technique' => '1',
+                        'Graphique' => '2',
+                ],
+            'expanded' => true
+            ])
             ->add('aboutme')
         ;
     }
